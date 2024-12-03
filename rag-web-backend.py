5f2e193588ff49f7cd6c358be367bafc54613879
Vid116 <118820302+Vid116@users.noreply.github.com>
2024-12-03 14:36:14 +0700

    Add files via upload

--- rag-web-backend.py
+++ rag-web-backend.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_websockets=True
+    #allow_websockets=True
 )
 
 # Additional middleware for WebSocket headers
